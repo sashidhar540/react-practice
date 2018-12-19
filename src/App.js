faa7e56a4be93378f2eddd8d862c1594df8ee6ef
ksashidh <kunapareddi_sashidhar@optum.com>
2018-12-19 18:03:36 +0530

    Practice day 1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Table, {MySection} from './Table';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        const characters = [
+            {
+                'name': 'Sasi',
+                'job': 'Software Engineer'
+            },
+            {
+                'name': 'Mac',
+                'job': 'Bouncer'
+            },
+            {
+                'name': 'Dee',
+                'job': 'Aspring actress'
+            },
+            {
+                'name': 'Dennis',
+                'job': 'Bartender'
+            }
+        ];
+        return(
+            <div className="container">
+                <Table characterData={characters}/>
+                <MySection/>
+            </div>
+        );
+    }
 }
 
 export default App;
